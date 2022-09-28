3dc2679f90aef64f47d259e5e4e894012cd601d1
Terrinia <helderlicht@live.nl>
2022-09-28 20:42:51 +0200

    first commit of code

---
+++ N3plus1/N3plus1.py
@@ -0,0 +1,16 @@
+
+
+number = input("please enter a number:")
+
+steps = 0
+
+while number => 1
+      if number == uneven
+      number = number x 3 + 1
+      steps = steps +1
+      else
+          number = number % 2
+          steps = steps + 1
+          break
+      print(number)
+      print(steps):
